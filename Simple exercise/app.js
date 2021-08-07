27124d236dcc246e92300a7f60b397c735f9ed89
Henrique Souza <72718207+henrique-souza@users.noreply.github.com>
2021-08-07 18:33:49 -0300

    Update app.js

--- Simple exercise/app.js
+++ Simple exercise/app.js
@@ -1,1 +1,15 @@
 'use strict'
+
+const switcher = document.querySelector('.button');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme')
+
+    var className = document.body.className;
+    if (className == "light-theme") {
+        this.textContent = "Escuro";
+    } else {
+        this.textContent = "Claro";
+    }
+    console.log('Nome da classe atual: ' + className);
+});
