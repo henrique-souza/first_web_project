47f7c3755e2b97fcf8a831d8f63320209340c50d
Henrique Souza <h.s.s_henrique@hotmail.com>
2021-09-17 09:19:55 -0300

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -6,10 +6,13 @@ switcher.addEventListener('click', function() {
     document.body.classList.toggle('dark-theme')
 
     var className = document.body.className;
-    if (className == "light-theme") {
+        
+        if (className == "light-theme") {
         this.textContent = "Escuro";
-    } else {
+        } 
+        else {
         this.textContent = "Claro";
-    }
+        }
+        
     console.log('Nome da classe atual: ' + className);
 });
