e6df0132237e51d429816a6ac609b03985c21715
Henrique Souza <h.s.s_henrique@hotmail.com>
2021-09-17 09:02:37 -0300

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -7,9 +7,9 @@ switcher.addEventListener('click', function() {
 
     var nomeDaClasse = document.body.nomeDaClasse;
     if (nomeDaClasse == "light-theme") {
-        this.textoDoBotao = "Escuro";
+        this.textContent = "Escuro";
     } else {
-        this.textoDoBotao = "Claro";
+        this.textContent = "Claro";
     }
     console.log('Nome da classe atual: ' + nomeDaClasse);
 });
